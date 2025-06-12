222fae9e7a9669054980ec7e8ffff6c5dd5c8009
loteque <loteque@bluebox.local>
2025-06-12 08:32:25 -0700

    add a func to get data as array, rename func that gets data as dict

--- Database/Database.gd
+++ Database/Database.gd
@@ -6,7 +6,7 @@ extends Resource
 @export var _data: Array[StructuredData]
 
 
-func compile_data() -> Dictionary:
+func compile_data_dictionary() -> Dictionary:
     var data = {}
     for item in _data:
         var key = str(item._id)
@@ -14,5 +14,15 @@ func compile_data() -> Dictionary:
     return data
 
 
-func get_data() -> Dictionary:
-    return compile_data()
+func get_data(data_index: int = -1) -> Array[StructuredData]:
+    var data
+    if data_index >= 0 and _data.size() > data_index:
+        data = _data[data_index]
+    else:
+        data = _data
+        
+    return data
+
+
+func get_data_as_dictionary() -> Dictionary:
+    return compile_data_dictionary()
