198686336cac1d12902e09690e6d4c6ff3aabeae
loteque <loteque@bluebox.local>
2025-06-13 09:40:02 -0700

    setup repo to be used as a submoduled plugin in a godot proj

--- demo/character_database/Character_data.gd
+++
@@ -1,5 +0,0 @@
-@tool
-extends SDClass
-class_name CharacterData
-
-@export var name: String
--- demo/character_screen.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@export var first_character: CharacterAdapter
-
-@export var second_character: CharacterAdapter
-
-@export var third_character: CharacterAdapter
